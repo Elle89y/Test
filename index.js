85bb50117464518cb61178a62e387af7d3416d53
Elle89y <118417533+Elle89y@users.noreply.github.com>
2022-11-17 17:04:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     //setInterval("renderCurrentTime()"); - 실시간 아님
     //setInterval("renderCurrentTime()", 1000); - 실시간, 1000ms단위로 바뀜 (1초)
     renderQuote();
+    renderRandomImage();
 });
 
 //현재 시간
@@ -30,3 +31,16 @@ function renderQuote() {
             $('#author').text(author);
         });
 }
+   
+    // 랜덤 사진 꾸미기
+    function renderRandomImage() {
+      let imageList = [];
+      // 이미지 개수를 변경하려면 i=5의 값을 이미지 개수만큼 바꿔주세요!
+      for (i = 0; i < 7; i++) {
+        imageList.push(i);
+      }
+      let imageListLength = imageList.length;
+      let randomImage = Math.floor(Math.random() * (imageListLength))+1
+      randomImage = `images/${randomImage}.jpg`
+      $(document.body).css("background-image", `url(${randomImage})`);
+    }
