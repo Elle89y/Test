c3041d12e4b338035958c630bfb5eae3307a2786
Elle89y <118417533+Elle89y@users.noreply.github.com>
2022-11-17 15:06:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     renderCurrentTime()
 	setInterval("renderCurrentTime()", 1000);
+    //setInterval("renderCurrentTime()"); - 실시간 아님
+    //setInterval("renderCurrentTime()", 1000); - 실시간, 1000ms단위로 바뀜 (1초)
     renderQuote();
 });
 
@@ -9,7 +11,10 @@ function renderCurrentTime() {
 let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
 fetch(url)
 .then(res => res.json()).then((data) => {
-    let datetime = data['datetime'].substr(11,8);
+    let datetime = data['datetime'].substr(11,5);
+    //let datetime = data['datetime'].substr(11,5); - 분까지만 나오는것
+    //let datetime = data['datetime'].substr(11,8); - 초까지 나옴
+    //let datatime = Date().substring(25,15) - 세계시간
     $('#time').text(datetime);
 });
 }
